81729c1ce106e7889d536dd9e3f52404baa54cad
Gaurav k <gauravk@in-gaurav.local>
2016-05-03 16:44:51 +0530

    separate the logic of downloading image from view controller

--- ImageApp/ImageApp/LoadImage.swift
+++ ImageApp/ImageApp/LoadImage.swift
@@ -1,13 +1,42 @@
-//
-//  LoadImage.swift
-//  ImageApp
-//
-//  Created by Gaurav k on 5/3/16.
-//  Copyright © 2016 Gaurav k. All rights reserved.
-//
-
 import UIKit
 
-class LoadImage: NSObject {
-
+class LoadImage {
+    init(){}
+    
+//    func loadingTask(url: NSURL, downloadedImage: UIImageView, activityController: UIActivityIndicatorView, errorInfoLable: UILabel) -> NSURLSessionDataTask {
+//        
+////        let alert = AlertMsg()
+//        let lableMsg = SetErrorMsgToLable()
+//        let request = NSURLRequest(URL: url)
+//        let session = NSURLSession.sharedSession()
+//        
+//        return session.dataTaskWithRequest(request, completionHandler: { (data, responce, error) in
+//            dispatch_async(dispatch_get_main_queue(),{
+//                
+//                if let imageData = data {
+//                    if let myImage = UIImage(data: imageData){
+//                        downloadedImage.image = myImage
+//                        print("if is here")
+//                        activityController.stopAnimating()
+//                    } else {
+//                        print("Else is here")
+////                        alert.errorMsg(activityController, msg: "Invalid url.. Please check your url")
+//                        lableMsg.showErr(errorInfoLable, msg: "Invalid")
+//
+//                    }
+//                }
+//            })
+//        })
+//    }
+    
+    func load(data: NSData?) -> UIImage?{
+        if let imageData = data {
+            if let myImage = UIImage(data: imageData){
+                return myImage
+            } else {
+                return nil
+            }
+        }
+        return nil
+    }
 }
