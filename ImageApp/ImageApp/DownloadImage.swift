68ba3051e279a39b452c553b6150b268b362d58c
gauravkhuperkar <gauravkh@thoughtworks.com>
2016-07-18 21:16:31 +0530

    Written downloadling logic in DownloadImage class.

---
+++ ImageApp/ImageApp/DownloadImage.swift
@@ -0,0 +1,45 @@
+import UIKit
+
+class LoadImage {
+    
+    init(){}
+    
+    func loadingTask(url: NSURL, downloadedImage: UIImageView, activityController: UIActivityIndicatorView, errorInfoLable: UILabel) -> NSURLSessionDataTask {
+        
+        let alert = AlertMsg()
+        let lableMsg = SetErrorMsgToLable()
+        let request = NSURLRequest(URL: url)
+        let session = NSURLSession.sharedSession()
+        
+        return session.dataTaskWithRequest(request, completionHandler: { (data, responce, error) in
+            dispatch_async(dispatch_get_main_queue(),{
+                
+                if let imageData = data {
+                    if let myImage = UIImage(data: imageData){
+                        downloadedImage.image = myImage
+                        print("if is here")
+                        activityController.stopAnimating()
+                    } else {
+                        print("Else is here")
+                        alert.errorMsg(activityController, msg: "Invalid url.. Please check your url")
+                        lableMsg.showErr(errorInfoLable, msg: "Invalid")
+                        
+                    }
+                }
+            })
+        })
+    }
+    
+    
+    func load(data: NSData?) -> UIImage?{
+        if let imageData = data {
+            if let myImage = UIImage(data: imageData){
+                return myImage
+            } else {
+                return nil
+            }
+        }
+        return nil
+    }
+    
+}
