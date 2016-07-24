c8296f4222ba2c895ac47fba63b529c088e4fbae
gauravkhuperkar <gauravkh@thoughtworks.com>
2016-07-24 20:05:39 +0530

    Deleted all unwanted code and introduce WKWebView to project through view controller

--- ImageApp/ImageApp/ViewController.swift
+++ ImageApp/ImageApp/ViewController.swift
@@ -1,44 +1,25 @@
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
-    @IBOutlet weak var urlTextField: UITextField!
-    @IBOutlet weak var downloadedImage: UIImageView!
-    @IBOutlet weak var errorLabel: UILabel!
+    @IBOutlet var containerView : UIView? = nil
+    var webView: WKWebView?
     
-    @IBAction func submit(sender: UIButton) {
-        startLoaderAnimation()
-        print(".............................")
-        if let text = urlTextField.text {
-            print(text)
-        }
-        
+    override func loadView() {
+        super.loadView()
+        self.webView = WKWebView()
+        self.view = self.webView
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let url = NSURL(fileURLWithPath: "http://www.google.com")
+        let req = NSURLRequest(URL: url)
+        self.webView?.loadRequest(req)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-    }
-    
-    // private methods
-    private func startLoaderAnimation() {
-        activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.WhiteLarge
-        activityIndicator.startAnimating()
-    }
-    
-    private func giveError(msg: String){
-        errorLabel.text = msg;
-    }
-    
-    private func alertError(activityController: UIActivityIndicatorView, msg: String) {
-        let viewController = ViewController()
-        activityController.stopAnimating()
-        let alert = UIAlertController(title: "Alert", message: msg, preferredStyle: UIAlertControllerStyle.Alert)
-        alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.Default, handler: nil))
-        viewController.presentViewController(alert, animated: true, completion: nil)
     }
 }
